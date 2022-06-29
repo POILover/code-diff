d17e47d0d130cf1a1a39754f425c3b8f1a60d7cd
wanglianjie <312822458@qq.com>
2022-06-29 17:32:28 +0800

    separate client and server logic

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 const http = require("http");
 const fs = require("fs");
-const Diff = require("diff");
-const Diff2Html = require("diff2html");
-hljs = require('highlight.js/lib/common');
+
 const server = http.createServer((req, res)=>{
-    console.log(req.url)
     res.setHeader('Content-Type', "text/html");
-    const diffDom1 = readAsyncFile("./src/file/b.ignore.html");
-    const diffDom2 = readAsyncFile("./src/file/b.html");
-    const diffResult = Diff.createPatch('fileName', diffDom1, diffDom2, "oldHeader", "newHeader");
-    const diffResultDom = Diff2Html.html(diffResult, {
-        drawFileList: true,
-        matching: "lines",
-        outputFormat: "side-by-side",
-      });
-    const style = readAsyncFile("./src/css/diff2html.min.css") + readAsyncFile("./src/css/github.css");
-    if(req.url!=="/"){
-        
-    }
-    // hljs.highlight(diffResultDom, {language:'html'}).value
-    // res.write(insertDomToTemplate(hljs.highlight(diffResultDom, {language:'html'}).value, style));
-    res.write(insertDomToTemplate(diffResultDom, style));
-    
+    const insertText1 = escapeChars(readAsyncFile("./src/file/b.ignore.html"));
+    const insertText2 = escapeChars(readAsyncFile("./src/file/b.html"));
+    res.write(insertDomToTemplate({insertText1, insertText2}));
+    res.end();
 });
 
 server.listen(9527, "127.0.0.1", () => {
@@ -29,17 +14,30 @@ server.listen(9527, "127.0.0.1", () => {
 })
 
 const template = readAsyncFile("./src/template.html");
-function insertDomToTemplate(dom, style){
+
+function insertDomToTemplate(obj){
+    const keys = Object.keys(obj);
+    const len = keys.length;
     return template.replace(/\{\{(.+?)\}\}/g, (_, content)=>{
-        if(content.trim() === "insertStyle"){
-            return style
-        }
-        if(content.trim() === "insertDom"){
-            return dom
+        for(let i=0;i<len;i++){
+            const key = keys[i];
+            if(content.trim() === key){
+                return obj[key]
+            }
         }
     })
 }
 
 function readAsyncFile(filePath){
     return fs.readFileSync(filePath, "utf8");
+}
+
+function escapeChars(str) {
+    str = str.replace(/&/g, '&amp;');
+    str = str.replace(/</g, '&lt;');
+    str = str.replace(/>/g, '&gt;');
+    str = str.replace(/'/g, '&acute;');
+    str = str.replace(/"/g, '&quot;');
+    str = str.replace(/\|/g, '&brvbar;');
+    return str;
 }
