57c4dc48186ffb99162a36fdebf995e93a0fac60
wanglianjie <312822458@qq.com>
2022-07-04 16:56:00 +0800

    fix: merge diff bug

--- public/js/index.js
+++ public/js/index.js
@@ -2,22 +2,22 @@ window.onload = () => {
 
     const text1 = getElementById("diff-text-1").innerText;
     const text2 = getElementById("diff-text-2").innerText;
-    const current = + getElementById("diff-file-current").innerText;
-    const total = + getElementById("diff-file-total").innerText;
+    const current = +getElementById("diff-file-current").innerText;
+    const total = +getElementById("diff-file-total").innerText;
 
     document.title = `Code Diff - ${current}`
 
     const prevDom = getElementById("prev-btn");
     const nextDom = getElementById("next-btn");
-    if(current === 1){
+    if (current === 1) {
         prevDom.setAttribute("disabled", `true`)
-        
-    }else{
+
+    } else {
         prevDom.setAttribute("href", `/index?page=${current - 1}`)
     }
-    if(current === total){
+    if (current === total) {
         nextDom.setAttribute("disabled", `true`)
-    }else{
+    } else {
         nextDom.setAttribute("href", `/index?page=${current + 1}`)
     }
 
@@ -46,7 +46,7 @@ function generateUnifiedHeader(fromFile = "from.js", toFile = "to.js") {
     return `--- ${fromFile}\n+++ ${toFile}\n@@ -1,86 +1,145 @@\n`;
 }
 
-function getElementById(id){
+function getElementById(id) {
     return document.getElementById(id);
 }
 /**
@@ -61,31 +61,25 @@ function getElementById(id){
  * 1. for common value, like '  aaa\n  bbbb\n  ccc\n',
  *    every piece of value before '\n' needs add '+ ' if added, '- ' if removed, '  ' if no change.
  * 
- * 2. if there is no '\n' in value, like '</script>', 
+ * 2. if there is no '\n' in the end, like '   ddd\n</script>', 
  *    we can consider it as the last line of file.
  *    If no change, seems add '  ' is enough,
- *    But if has change, there will be two values which have no '\n', so a '\n' is needed to add at the former one to make two lines in html.
- *    Simply we can always add a '\n' after the string in such situation.
- * 
- * So the common rule seems like splitValueArrayLength===1 ? prefix + value + '\n' : prefix + splitStr + '\n'.
+ *    But if change, there will be two values which have no '\n' in the end, so a '\n' is needed to add at the former one to make two lines in html.
+ *    Simply we can always add a '\n' after the both string in such situation.
  * 
  */
 function resolveDiffLinesResult(diffLinesResult) {
     return diffLinesResult
         .map((item) => {
-            let value = item.value;
-            const valueArr = value.split("\n");
-            valueArr.pop(); //delete tail's blank item, if last line, valueArr will contain nothing.
             const prefix = item.added ? "+ " : item.removed ? "- " : "  ";
-            if (valueArr.length===0) {
-                // last line
-                return `${prefix}${value}\n`;
-            } else {
-                // not last line
-                return valueArr
-                    .map(v => `${prefix}${v}\n`)
-                    .join("");
+            const valueArr = item.value.split("\n");
+            const valueArrLastStr = valueArr[valueArr.length - 1];
+            if (!valueArrLastStr.length) {
+                valueArr.pop(); // delete tail's blank item.
             }
+            return valueArr
+                .map(v => `${prefix}${v}\n`)
+                .join("");
         })
         .join("");
 }
